31443f8b753dac42deb37ae27206d900d39453ae
zhouchang <290621352@qq.com>
2019-01-16 11:37:41 +0800

    fix supplier wx bind

--- app/Http/Controllers/Supplier/Auth/WechatController.php
+++ app/Http/Controllers/Supplier/Auth/WechatController.php
@@ -28,6 +28,16 @@ class WechatController extends Controller
     public function bind(SupplierUser $user, ErpRequest $request)
     {
         $this->authorizeUrl();
-        dd($user);
+
+        if ( !$user->hasBind()) {
+            $oauthUser = session('wechat.oauth_user.default');
+            $user->wechat()->create([
+                'openid' => $oauthUser->getId(),
+                'avatar' => $oauthUser->getAvatar(),
+                'nickname' => $oauthUser->getNickname(),
+            ]);
+            return view('wechat.success', ['message' => '绑定成功！']);
+        }
+        return view('wechat.success', ['message' => '您已经绑定，无需再次绑定！']);
     }
 }
